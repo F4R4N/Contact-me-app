ffacd523326bea19598646cf6bab21ce7db39e07
faran <farantgh@gmail.com>
2021-03-30 13:19:42 -0400

    added mass mail url to url paths

--- contactus/urls.py
+++ contactus/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateForm, AdminContactReader
+from .views import CreateForm, AdminContactReader, AdminSendMassEmail
 
 app_name = "contact"
 
 urlpatterns = [
 	path("contact/", CreateForm.as_view()),
 	path("admin/contact/", AdminContactReader.as_view()),
+	path("admin/mass_mail/", AdminSendMassEmail.as_view()),
 ]
