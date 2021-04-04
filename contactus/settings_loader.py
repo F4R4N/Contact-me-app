1d9a08226f3defad8c083a65a7aa21b5810fcc19
faran <farantgh@gmail.com>
2021-04-04 11:13:34 +0430

    added settings_loader to have default settings and modify that based on user settings

---
+++ contactus/settings_loader.py
@@ -0,0 +1,25 @@
+from rest_framework.response import Response
+from rest_framework import status
+DEFAULT_CONTACT_US_SETTINGS = {
+	'APP_NAME': None,
+	'SEND_MAIL': False,
+	'MAIL_SUBJECT': " Contact Us ",
+	'MESSAGE': "dear '%s',\n we got your email. we will response as soon as possible.\n\nBest Regards '%s'"
+}
+
+try:
+	from django.conf.settings import CONTACT_US_SETTINGS
+except ImportError:
+	print("ERROR: No CONTACT_US_SETTINGS Found At Projects settings.py")
+
+fields = ["APP_NAME", "SEND_MAIL", "MAIL_SUBJECT", "MESSAGE"]
+
+for field in fields:
+	if field in CONTACT_US_SETTINGS:
+		DEFAULT_CONTACT_US_SETTINGS[field] = CONTACT_US_SETTINGS[field]
+
+try:
+	from django.conf.settings import EMAIL_USE_TLS, EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+except ImportError:
+	print("ERROR: No Email Configuration Found in projects settings.py."+'configure : "EMAIL_USE_TLS", "EMAIL_HOST", "EMAIL_PORT", "EMAIL_HOST_USER", "EMAIL_HOST_PASSWORD"'+ "or Set SEND_MAIL=False")
+
